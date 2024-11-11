5e83b91baf90a8fac8c143994cc025281c2f53b3
Sandeep K <blackgen629@gmail.com>
2024-11-11 21:56:04 +0530

    fixed swagger err

--- swagger.ts
+++ swagger.ts
@@ -3,7 +3,7 @@ import swaggerJsdoc from 'swagger-jsdoc';
 import { Options } from 'swagger-jsdoc';
 
 // import the BASE_URL from the process.env object or .env file
-const BASE_URL = process.env.BASE_URL || 'http://localhost:8000/v1';
+const BASE_URL = process.env.BASE_URL || 'http://localhost:8000';
 
 const options: Options = {
   definition: {
@@ -20,7 +20,7 @@ const options: Options = {
       }
     ],
   },
-  apis: ['./app.ts'],
+  apis: ['./app.ts', './dist/app.js'],
 };
 
 const swaggerSpec = swaggerJsdoc(options);
