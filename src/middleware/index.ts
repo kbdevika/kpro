daa946c9ce5c650aecd90309e1f54cf059e41df1
Sandeep K <blackgen629@gmail.com>
2024-11-28 16:22:02 +0530

    authentication-fix-1

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,8 +1,9 @@
 import { NextFunction, Request, Response } from "express";
 import admin from "../firebase.config";
+import prisma from "../prisma.config";
 
 class Middleware{
-    async decodeToken(req: Request, res: Response, next: NextFunction){
+    async decodeToken(req: any, res: Response, next: NextFunction){
         const token = req.headers.authorization?.split(' ')[1];
 
         if (!token) {
@@ -11,10 +12,38 @@ class Middleware{
         }
 
         try {
-            await admin.auth().verifyIdToken(token);
+            const decodedToken = await admin.auth().verifyIdToken(token);
+
+            let user = await prisma.user.findUnique({
+                where: { id: decodedToken.uid }
+              });
+          
+            if (!user) {
+                user = await prisma.user.create({
+                    data: { 
+                    id: decodedToken.uid
+                }
+              });
+
+            if(decodedToken.phone_number){
+                await prisma.userSetting.create({
+                    data: {
+                      userId: decodedToken.uid,
+                      key: "phone",
+                      value: decodedToken.phone_number
+                    }
+                  });
+                }  
+            }
+
+            // Add user info to req.user
+            req.user = {
+                id: decodedToken.uid,
+            };
+
             next();
-        } catch (error) {
-            res.status(403).json({ error: 'Invalid or expired token' });
+        } catch (error: any) {
+            res.status(403).json({ error: 'Invalid or expired token' + error.message });
             return;
         }
 
