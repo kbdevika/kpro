8b4afe83e4de9d1ce40ab4e8224a77cdcb0fa38c
Sandeep K <blackgen629@gmail.com>
2024-11-29 06:09:11 +0530

    test-commit

---
+++ src/routes/v1/test.routes.ts
@@ -0,0 +1,45 @@
+import express from 'express';
+import prisma from '../../config/prisma.config';
+import Store from '../../types/ondcStore.type';
+
+const testRouter = express.Router()
+
+testRouter.post('/', async (req: any, res: any) => {
+    try {
+        // Fetch from the external API if not present
+        const response = await fetch('https://ondc-api.kiko.live/ondc-seller-v2/kiranaProSearch', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ pincode: req.body.pincode }),
+        });
+    
+        if (!response.ok) {
+          throw new Error(`Failed to fetch catalogue from API: ${response.statusText}`);
+        }
+    
+        const data = await response.json();
+        const storeData: Store[] = data;
+    
+        // Store the fetched catalogue in the database
+        const newCatalogue = await prisma.catalogue.upsert({
+          where: {
+            pincode: req.body.pincode,  // Assuming 'pincode' is unique for each catalogue
+          },
+          update: {
+            jsonData: storeData,  // If the catalogue exists, update its jsonData
+          },
+          create: {
+            pincode: req.body.pincode,  // If the catalogue doesn't exist, create a new one
+            jsonData: storeData,
+          },
+        });
+        return data;
+    
+      } catch (error: any) {
+        throw new Error(`Error fetching catalogue: ${error.message}`);
+      }
+})
+
+export default testRouter;
